a439b8811cea94231918b234177bb9b2c8faa02e
李正钧 <lzj@umshz.com>
2019-02-20 17:48:33 +0800

    多数据源配置

---
+++ chapter2/multi-datasource-demo/src/main/java/com/grouk/multidatasourcedemo/MultidatasourcedemoApplication.java
@@ -0,0 +1,71 @@
+package com.grouk.multidatasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * 屏蔽spring-boot的自动配置的，手动实现多数据源配置
+ *
+ * @author lizhengjun
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class})
+@Slf4j
+public class MultidatasourcedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultidatasourcedemoApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties("foo.datasource")
+    public DataSourceProperties fooDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource fooDataSource() {
+        DataSourceProperties sourceProperties = fooDataSourceProperties();
+        log.info("foo datasource: {}" + sourceProperties.getUrl());
+        return sourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+
+
+    @Bean
+    @ConfigurationProperties("bar.datasource")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties sourceProperties = barDataSourceProperties();
+        log.info("foo datasource: {}" + sourceProperties.getUrl());
+        return sourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        return new DataSourceTransactionManager(barDataSource);
+    }
+}
---
+++ chapter2/multi-datasource-demo/src/test/java/com/grouk/multidatasourcedemo/MultidatasourcedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grouk.multidatasourcedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultidatasourcedemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
