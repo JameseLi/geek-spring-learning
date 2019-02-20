a7c115aff7d93a60e6f23c77c8c16cf98a15df3c
李正钧 <lzj@umshz.com>
2019-02-20 11:00:29 +0800

    但数据源配置

--- chapter1/hello-spring/src/main/java/com/grouk/hellospring/HelloSpringApplication.java
+++ chapter1/hello-spring/src/main/java/com/grouk/hellospring/HelloSpringApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author lizhengjun
+ */
 @SpringBootApplication
 @RestController
 public class HelloSpringApplication {
